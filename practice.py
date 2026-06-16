3c5f7078c6158c2fc14a2080243a73a225e275d8
Ramzanshoukat52 <rumishoukat52@gmail.com>
2026-06-16 13:43:37 +0500

    First Practice

---
+++ practice.py
@@ -0,0 +1,20 @@
+book = "Python "
+print("Book Title:" + book)
+bookID = 5403
+print("Book ID:" , bookID)
+price = 549
+print("Book Price:" ,price)
+name = input("Enter the name:")
+print("welcome pythone course Enrollment" + name)
+
+# book = "Python"
+# print("Book Title: " + book)
+
+# bookID = 5403
+# print("Book ID:", bookID)
+
+# price = 549
+# print("Book Price:", price)
+
+# name = input("Enter the name: ")
+# print("Welcome Python Course Enrollment " + name)
