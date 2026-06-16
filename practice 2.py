850821fb7a60d3c7850163e11db57c0d22d8d779
Ramzanshoukat52 <rumishoukat52@gmail.com>
2026-06-16 18:44:21 +0500

    Practice 2.

---
+++ practice 2.py
@@ -0,0 +1,10 @@
+info = "Python is a Brilliant Progtraming language"
+print(info)
+print(len(info))
+print(type(info))
+print(info.upper())
+print(info.swapcase())
+print(info.title())
+print(info[4:30])
+for i in info:
+    print(i)
